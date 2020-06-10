73dcc3ba373ab3e96ad556bde1ead8283a2f6b0e
Max <max.lubda@gmail.com>
2020-06-10 17:18:59 +0200

    Preprocesing added space after dots

--- Naive Bayes/Preprocessing.py
+++ Naive Bayes/Preprocessing.py
@@ -4,21 +4,24 @@ import tensorflow as tf
 import pandas as pd
 import os
 import nltk
-
+from pathlib import Path
+import re
 import tensorflow_datasets as tfds
 
-#data = tfds.load("cnn_dailymail") 
-
+# Initialize the dataframe used to store Articles in col. 0 and Summaries in col. 1
 index = np.linspace(0, 509, 510)
 business_data = pd.DataFrame(columns=["Article", "Summary"], index=index)
 business_data = business_data.fillna(0)
 
-business_article_dir = r'C:\Users\user\Documents\Uni\Winfo\1. Semester\DataScience1\Summarizer\24984_32267_bundle_archive\BBC News Summary\News Articles\business'
-business_summary_dir = r'C:\Users\user\Documents\Uni\Winfo\1. Semester\DataScience1\Summarizer\24984_32267_bundle_archive\BBC News Summary\Summaries\business'
+# Relative path of dataset
+rootpath = Path.cwd()
+articlepath = Path.joinpath(rootpath, r"Dataset 1 (BBC)\News Articles\business")
+summarypath = Path.joinpath(rootpath, r"Dataset 1 (BBC)\Summaries\business")
 
 
+# Insert data from .txt files into the dataframe
 count = 0
-for entry in os.scandir(business_article_dir):
+for entry in os.scandir(articlepath):
     text_file = open(entry, "r")
     raw_text = text_file.read()
     business_data.iloc[count, 0] = raw_text
@@ -27,7 +30,7 @@ for entry in os.scandir(business_article_dir):
     count += 1
 
 count = 0
-for entry in os.scandir(business_summary_dir):
+for entry in os.scandir(summarypath):
     text_file = open(entry, "r")
     raw_text = text_file.read()
     business_data.iloc[count, 1] = raw_text
@@ -35,12 +38,17 @@ for entry in os.scandir(business_summary_dir):
     
     count += 1
 
-#print(business_data.head())
-#print(business_data.tail())
+# Add spaces after the dots in the summaries (They are missing in the original summaries)
+for i in range(510):
+    business_data.iloc[i, 1] = re.sub(r"\D\.", ". ", business_data.iloc[i, 1], 0)
 
+
+# Tokenize the data sentence by sentence
 for i in range(510):
     for j in range(2):
         business_data.iloc[i, j] = nltk.sent_tokenize(str(business_data.iloc[i, j]))
+
+
 """   
 for i in range(510):
     for sentence in business_data.iloc[i, 0]:
