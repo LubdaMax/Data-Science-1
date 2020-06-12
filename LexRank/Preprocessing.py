3993045bd2f676ddd1bfab4c867eea8cfdb3a943
Lenila27 <anlenkis@gmail.com>
2020-06-12 19:12:12 +0200

    Setup dataframe Insert all articles into dataframe (not yet tested)

--- LexRank/Preprocessing.py
+++ LexRank/Preprocessing.py
@@ -20,23 +20,21 @@ articlepath_folder = Path.joinpath(rootpath, r"Dataset 1 (BBC)\News Articles")
 summarypath_folder = Path.joinpath(rootpath, r"Dataset 1 (BBC)\Summaries")
 
 
-
 # Insert data (ALL articles) from .txt files into the dataframe
-#topics = ["business", "entertainment","politics","sport","tech"]
+#categories = ["business", "entertainment","politics","sport","tech"]
 
 # Insert data (only business articles) from .txt files into the dataframe
-topics = ["business"]
+categories = ["business"]
 
 count = 0
-for topic in topics:
-    articlepath = articlepath_folder + "\\" + topic
+for category in categories:
+    articlepath = articlepath_folder + "\\" + category
     for entry in os.scandir(articlepath):
         text_file = open(entry, "r")
         raw_text = text_file.read()
         total_data.iloc[count, 0] = raw_text
-        total_data.iloc[count,1] = topic
+        total_data.iloc[count,1] = category
         text_file.close()
 
         count += 1
-
 
