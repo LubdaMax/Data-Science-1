d9aba4c5a2b83c741fd1774f8cc4d5731f1e4ade
Max <max.lubda@gmail.com>
2020-06-24 12:53:30 +0200

    Evaluation working

--- Naive Bayes/evaluate.py
+++ Naive Bayes/evaluate.py
@@ -18,15 +18,19 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 
 rootpath = Path.cwd()
-openfile = open(Path.joinpath(rootpath, r"Gauss_trained_4_no_overview"), 'rb')
+openfile = open(Path.joinpath(rootpath, r"Gauss_trained_8_no_overview"), 'rb')
 GaussNB = pickle.load(openfile)
 openfile.close()
 
-open_wiki = open(Path.joinpath(rootpath, r"Wikihow\wiki_data_indep_4"), 'rb')
+open_wiki = open(Path.joinpath(rootpath, r"Wikihow\wiki_data_indep_8_no_overview"), 'rb')
 wiki_data = pickle.load(open_wiki)
 open_wiki.close()
 
-open_cnn_article = open(Path.joinpath(rootpath, r"cnn_indep_4"), 'rb')
+open_wiki_summ = open(Path.joinpath(rootpath, r"Wikihow\wiki_partial_summaries"), 'rb')
+wiki_summary = pickle.load(open_wiki_summ)
+open_wiki_summ.close()
+
+open_cnn_article = open(Path.joinpath(rootpath, r"cnn_indep_8"), 'rb')
 cnn_data = pickle.load(open_cnn_article)
 open_cnn_article.close()
 
@@ -54,14 +58,7 @@ def get_summ_wiki(dictionary):
     Summaries = []
 
     for i in range(len(dictionary)):
-        Summary = ""
-
-        for s in range(dictionary["Article{0}".format(i)].shape[0]):
-            if dictionary["Article{0}".format(i)].iloc[s, 1] == 1:
-                Summary += dictionary["Article{0}".format(i)].iloc[s, 0]
-                Summary += " "
-
-        Summary = Summary[:-1]
+        Summary = dictionary["Summary{0}".format(i)]
         Summaries.append(Summary)
     return Summaries
 
@@ -104,33 +101,43 @@ def create_summ_wiki(dictionary):
     return Summaries
 
 
-def eval_summ(cnn_summary, cnn_data, wiki_data):
+def eval_summ_bayes(cnn_summary, cnn_data, wiki_data, wiki_summary):
     cnn_summ = get_summ_cnn(cnn_summary)
     cnn_generated_summ = create_summ_cnn(cnn_data)
-    wiki_summ = get_summ_wiki(wiki_data)
+    wiki_summ = get_summ_wiki(wiki_summary)
     wiki_generated_summ = create_summ_wiki(wiki_data)
     rouge = Rouge()
-    """
-    cnn_scores = np.array([])
     
+    cnn_score_frame = pd.DataFrame(columns=["r1-f", "r1-p", "r1-r", "r2-f", "r2-p", "r2-r","rl-f", "rl-p", "rl-r"])
     for i in range(len(cnn_summ)):
         #score = recall(nltk.sent_tokenize(cnn_summ[i]), nltk.sent_tokenize(cnn_generated_summ[i]))
-        print(cnn_generated_summ[i])
-        score = rouge.get_scores(cnn_summ[i], cnn_generated_summ[i])
-        cnn_scores = np.append(cnn_scores, score)
-    cnn_mean_score = cnn_scores.mean()
-    print(cnn_mean_score)
-    """
-    wiki_scores = np.array([])
+        if (len(cnn_summ[i]) and len(cnn_generated_summ[i])) > 0: 
+            try: 
+                score = rouge.get_scores(cnn_summ[i], cnn_generated_summ[i])
+
+                cnn_score_frame = cnn_score_frame.append(pd.DataFrame({'r1-f': score[0]['rouge-1']['f'], 'r1-p': score[0]['rouge-1']['p'], 'r1-r': score[0]['rouge-1']['r'], 'r2-f': score[0]['rouge-2']['f'], 'r2-p': score[0]['rouge-2']['p'], 'r2-r': score[0]['rouge-2']['r'], 'rl-f': score[0]['rouge-l']['f'], 'rl-p': score[0]['rouge-l']['p'], 'rl-r': score[0]['rouge-l']['r']}, index=[0]), ignore_index=True)
+            except:
+                print("summary: ", cnn_summ[i])
+                print("generated: ", cnn_generated_summ[i])
+                print("------------------------------------------------------------------------------------------------------------------------")
+
+    wiki_score_frame = pd.DataFrame(columns=["r1-f", "r1-p", "r1-r", "r2-f", "r2-p", "r2-r","rl-f", "rl-p", "rl-r"])
     for i in range(len(wiki_summ)):
         #score = recall(nltk.sent_tokenize(cnn_summ[i]), nltk.sent_tokenize(cnn_generated_summ[i]))
-        print(wiki_summ[i])
-        score = rouge.get_scores(wiki_summ[i], wiki_generated_summ[i])
-        wiki_scores = np.append(wiki_scores, score)
+        if (len(wiki_summ[i]) and len(wiki_generated_summ[i])) > 0: 
+            score = rouge.get_scores(wiki_summ[i], wiki_generated_summ[i])
 
-    print(wiki_scores)    
-    wiki_mean_score = wiki_scores.mean()
+            wiki_score_frame = wiki_score_frame.append(pd.DataFrame({'r1-f': score[0]['rouge-1']['f'], 'r1-p': score[0]['rouge-1']['p'], 'r1-r': score[0]['rouge-1']['r'], 'r2-f': score[0]['rouge-2']['f'], 'r2-p': score[0]['rouge-2']['p'], 'r2-r': score[0]['rouge-2']['r'], 'rl-f': score[0]['rouge-l']['f'], 'rl-p': score[0]['rouge-l']['p'], 'rl-r': score[0]['rouge-l']['r']}, index=[0]), ignore_index=True)
+
+    return wiki_score_frame, cnn_score_frame
     
-
-
-eval_summ(cnn_summary, cnn_data, wiki_data)
+    
+wiki_score, cnn_score = eval_summ_bayes(cnn_summary, cnn_data, wiki_data, wiki_summary)
+print(wiki_score.head(), wiki_score.shape)
+print("r1-f mean: ", wiki_score["r1-f"].mean())
+print("r2-f mean: ", wiki_score["r2-f"].mean())
+print("rl-f mean: ", wiki_score["rl-f"].mean())
+print(cnn_score.head(), cnn_score.shape)
+print("r1-f mean: ", cnn_score["r1-f"].mean())
+print("r2-f mean: ", cnn_score["r2-f"].mean())
+print("rl-f mean: ", cnn_score["rl-f"].mean())
