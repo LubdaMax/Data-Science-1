12201e5494c24b6f7c31074f9444d76c5f9fef53
Max <max.lubda@gmail.com>
2020-06-26 09:38:55 +0200

    stats

--- Evaluation/descriptive_stats.py
+++ Evaluation/descriptive_stats.py
@@ -51,6 +51,15 @@ cnn_bayes_summary = pickle.load(open_cnn_bayes_summary)
 open_cnn_bayes_summary.close()
 
 
+open_wiki_lex_summary = open(Path.joinpath(rootpath, r"LexRank\wiki_TRoutput_summ_dict"), 'rb')
+wiki_lex_summary = pickle.load(open_wiki_lex_summary)
+open_wiki_lex_summary.close()
+
+open_cnn_lex_summary = open(Path.joinpath(rootpath, r"LexRank\cnn_TRoutput_summ_dict"), 'rb')
+cnn_lex_summary = pickle.load(open_cnn_lex_summary)
+open_cnn_lex_summary.close()
+
+
 
 def get_summ(dictionary):
     length = np.array([])
@@ -87,19 +96,22 @@ def get_bayes_summaries(summaries):
     return length
 
 
-def get_hist(ws, cs, wa, ca, wbs, cbs):
+def get_hist(ws, cs, wa, ca, wbs, cbs, wls, cls):
     fig, ax = plt.subplots(2, 2)
     fig.suptitle("Lenght of Summary and Aritcle")
-    ax[0,0].hist(wa)
+    ax[0,0].hist(wa, bins=np.linspace(wa.min(), wa.max(), 30))
+    ax[0,0].axvline(wa.mean(), color='k', linestyle='dashed', linewidth=1)
     ax[0,0].set_title("Wikihow Articles")
     ax[0,0].set_xlabel("#Sentences, Mean: 33.84")
-    ax[0,1].hist(ca)
+    ax[0,1].hist(ca, bins=np.linspace(ca.min(), ca.max(), 30))
+    ax[0,1].axvline(ca.mean(), color='k', linestyle='dashed', linewidth=1)
     ax[0,1].set_title("CNN Articles")
     ax[0,1].set_xlabel("#Sentences, Mean: 37.13")
-    ax[1,0].boxplot([ws, wbs])
-    ax[1,1].boxplot([cs, cbs])
+    ax[1,0].boxplot([ws, wbs, wls], labels=["Original", "Bayes", "LexRank"])
+    ax[1,0].set_title("#Sentences per Summary (WikiHow)")
+    ax[1,1].boxplot([cs, cbs, cls], labels=["Original", "Bayes", "LexRank"])
+    ax[1,1].set_title("#Sentences per Summary (CNN)")
     plt.show()
-
 
 
 wiki_summ_len = get_summ_wiki(wiki_summary)
@@ -108,13 +120,15 @@ wiki_art_len = get_summ_art(wiki_data)
 cnn_art_len = get_summ_art(cnn_data)
 wiki_bayes_summ_len = get_bayes_summaries(wiki_bayes_summary)
 cnn_bayes_summ_len = get_bayes_summaries(cnn_bayes_summary)
+wiki_lex_summ_len = get_summ(wiki_lex_summary)
+cnn_lex_summ_len = get_summ(cnn_lex_summary)
 cnn_min_sum, cnn_max_sum, cnn_mean_summ = cnn_summ_len.min(), cnn_summ_len.max(), cnn_summ_len.mean()
 wiki_min_sum, wiki_max_sum, wiki_mean_summ = wiki_summ_len.min(), wiki_summ_len.max(), wiki_summ_len.mean()
 cnn_min, cnn_max, cnn_mean = cnn_art_len.min(), cnn_art_len.max(), cnn_art_len.mean()
 wiki_min, wiki_max, wiki_mean = wiki_art_len.min(), wiki_art_len.max(), wiki_art_len.mean()
 
 
-get_hist(wiki_summ_len, cnn_summ_len, wiki_art_len, cnn_art_len, wiki_bayes_summ_len, cnn_bayes_summ_len)
+get_hist(wiki_summ_len, cnn_summ_len, wiki_art_len, cnn_art_len, wiki_bayes_summ_len, cnn_bayes_summ_len, wiki_lex_summ_len, cnn_lex_summ_len)
 print(cnn_min_sum, cnn_max_sum, cnn_mean_summ)
 print(wiki_min_sum, wiki_max_sum, wiki_mean_summ)
 
